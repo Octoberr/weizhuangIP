1178e10a1776b4234420a691fbe6975bce1188fc
Allen <zengyinghao@bestwise.cc>
2017-08-01 19:36:18 +0800

    modify config and and logging

---
+++ feichangzun/Utils/config.py
@@ -0,0 +1,5 @@
+import yaml
+
+f = open('./config/config.yaml')
+
+config = yaml.load(f)
--- feichangzun/main.py
+++ feichangzun/main.py
@@ -3,19 +3,34 @@ import getflightdata
 
 import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
+import logging
+from cmreslogging.handlers import CMRESHandler
+from Utils.config import config
 
+
+def config_logging():
+    esConf = config['es']
+    logging.basicConfig(level=logging.DEBUG)
+    handler = CMRESHandler(hosts=[{'host': esConf['host'], 'port': esConf['port']}],
+                            auth_type=CMRESHandler.AuthType.NO_AUTH,
+                            es_index_name=esConf['index'],
+                            es_additional_fields={'App': 'AutoPushOrder', 'Environment': 'Dev'})
+    log = logging.getLogger()
+    log.addHandler(handler)
+    log.setLevel(level=logging.DEBUG)
 
 def main():
-    print(datetime.datetime.now(), "Crawling the start")
+    config_logging()
+    logging.info(datetime.datetime.now(), "Crawling the start")
     fp = getflight.FCZPAC()
     gt = getflightdata.GETFLIGHTDATA()
     fp.start()
     gt.getFlightData()
-    print(datetime.datetime.now(), "Crawling the end")
+    logging.info(datetime.datetime.now(), "Crawling the end")
 
 
 if __name__ == '__main__':
-    print(datetime.datetime.now(), "The program has started")
+    logging.info(datetime.datetime.now(), "The program has started")
     scheduler = BlockingScheduler()
     main()
     scheduler.add_job(main, 'interval', hours=12)
